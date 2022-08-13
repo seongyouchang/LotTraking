f82807d714375672ec65660fd8b1f76d145e2550
hb <gksqls2047@naver.com>
2022-08-13 20:59:48 +0900

    hb

--- Form_list/RowMaster.Designer.cs
+++ Form_list/RowMaster.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tstUserId = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRowName = new System.Windows.Forms.TextBox();
+            this.txtiLROW = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textRowAccount = new System.Windows.Forms.TextBox();
+            this.textRowStock = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GridRowMaster = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridRowMaster)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textRowStock);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textRowAccount);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtiLROW);
+            this.groupBox1.Controls.Add(this.txtRowName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tstUserId);
+            this.groupBox1.Size = new System.Drawing.Size(899, 125);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.GridRowMaster);
+            this.groupBox2.Size = new System.Drawing.Size(899, 325);
+            // 
+            // tstUserId
+            // 
+            this.tstUserId.AutoSize = true;
+            this.tstUserId.Location = new System.Drawing.Point(219, 56);
+            this.tstUserId.Name = "tstUserId";
+            this.tstUserId.Size = new System.Drawing.Size(67, 15);
+            this.tstUserId.TabIndex = 4;
+            this.tstUserId.Text = "원자재명";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "원자재번호";
+            // 
+            // txtRowName
+            // 
+            this.txtRowName.Location = new System.Drawing.Point(292, 53);
+            this.txtRowName.Name = "txtRowName";
+            this.txtRowName.Size = new System.Drawing.Size(100, 25);
+            this.txtRowName.TabIndex = 7;
+            // 
+            // txtiLROW
+            // 
+            this.txtiLROW.Location = new System.Drawing.Point(106, 53);
+            this.txtiLROW.Name = "txtiLROW";
+            this.txtiLROW.Size = new System.Drawing.Size(100, 25);
+            this.txtiLROW.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "원자재 거래처";
+            // 
+            // textRowAccount
+            // 
+            this.textRowAccount.Location = new System.Drawing.Point(506, 53);
+            this.textRowAccount.Name = "textRowAccount";
+            this.textRowAccount.Size = new System.Drawing.Size(100, 25);
+            this.textRowAccount.TabIndex = 10;
+            // 
+            // textRowStock
+            // 
+            this.textRowStock.Location = new System.Drawing.Point(756, 56);
+            this.textRowStock.Name = "textRowStock";
+            this.textRowStock.Size = new System.Drawing.Size(100, 25);
+            this.textRowStock.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(633, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "원자재 입고일자";
+            // 
+            // GridRowMaster
+            // 
+            this.GridRowMaster.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridRowMaster.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GridRowMaster.Location = new System.Drawing.Point(3, 22);
+            this.GridRowMaster.Name = "GridRowMaster";
+            this.GridRowMaster.RowHeadersWidth = 51;
+            this.GridRowMaster.RowTemplate.Height = 27;
+            this.GridRowMaster.Size = new System.Drawing.Size(893, 299);
+            this.GridRowMaster.TabIndex = 0;
+            // 
+            // RowMaster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(899, 450);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "RowMaster";
+            this.Text = "품목 마스터";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GridRowMaster)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Label tstUserId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textRowStock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textRowAccount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtiLROW;
+        private System.Windows.Forms.TextBox txtRowName;
+        private System.Windows.Forms.DataGridView GridRowMaster;
     }
 }
--- Form_list/RowMaster.cs
+++ Form_list/RowMaster.cs
@@ -14,9 +14,119 @@ namespace Form_list
 {
     public partial class RowMaster : BaseChlidForm
     {
+        private SqlConnection Connect; // 데이터베이스에 접속 하는 정보를 관리하는 클래스.
+
+        // 2. SELECT (조회)를 실행하여 데이터베이스에서 데이터를 받아오는 클래스.
+        private SqlDataAdapter Adapter;
+
+        // 3. Insert, Update, Delete 명령을 전달할 클래스
+        private SqlTransaction tran; // 데이터베이스 데이터 관리 권한 부여.
+        private SqlCommand cmd;
+
         public RowMaster()
         {
             InitializeComponent();
         }
+        private void Form03_UserMaster_Load(object sender, EventArgs e)
+        {
+            /*****************기본 그리드 내역 셋팅 *******************/
+            DataTable dtGrid = new DataTable();
+            dtGrid.Columns.Add("iLROW", typeof(float));                    // 원자재 번호
+            dtGrid.Columns.Add("ROWNAME", typeof(char));            //  원자재 명
+            dtGrid.Columns.Add("ROWACCOUNT", typeof(char));       //  원자재 거래처
+            dtGrid.Columns.Add("ROWSTOCK", typeof(DateTime));      // 원자재 입고일자
+            dtGrid.Columns.Add("ROWEA", typeof(float));                   //원자재 재고 개수
+
+
+
+            // 빈 컬럼 테이블 그리드에 매칭. (DataSource : 테이블 형식의 데이터를 표현하는 속성)
+            GridRowMaster.DataSource = dtGrid;
+
+            // 그리드 컬럼 명칭(Text) 설정 // 번호로 지정 가능
+            GridRowMaster.Columns["iLROW"].HeaderText = "원자재 번호";
+            GridRowMaster.Columns["ROWNAME"].HeaderText = "원자재 명";
+            GridRowMaster.Columns["ROWACCOUNT"].HeaderText = "원자재 거래처";
+            GridRowMaster.Columns["ROWSTOCK"].HeaderText = "원자재 입고일자";
+            GridRowMaster.Columns["ROWEA"].HeaderText = "원자재 재고 개수";
+
+
+
+            // 컬럼의 폭 지정
+            GridRowMaster.Columns[0].Width = 100;
+            GridRowMaster.Columns[1].Width = 200;
+            GridRowMaster.Columns[2].Width = 200;
+            GridRowMaster.Columns[3].Width = 150;
+            GridRowMaster.Columns[4].Width = 00;
+
+
+            //// 컬럼의 수정 여부를 지정
+            GridRowMaster.Columns["iLROW"].ReadOnly = true;
+            GridRowMaster.Columns["ROWNAME"].ReadOnly = true;
+            GridRowMaster.Columns["ROWACCOUNT"].ReadOnly = true;
+            GridRowMaster.Columns["ROWSTOCK"].ReadOnly = true;
+            GridRowMaster.Columns["ROWEA"].ReadOnly = true;
+        }
+
+        // 메서드 오버라이드 (Override)
+        // virture로 상속을 허락 한 메서드를
+        // 각 화면에 맞는 기능으로 변경하여 메서드를 재 정의하고 사용하는 방식.
+        public override void Inquire()
+        {
+
+            if (DBHelper(false) == false) return;
+
+            try
+            {
+                // 사용자 정보 조회
+
+                // Adapter에 SQL 프로시져 이름과 접속 정보 등록.
+                Adapter = new SqlDataAdapter("BM_RowManager_S", Connect);
+                Adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                // 파라미터의 개수 별로 함수에 아규먼트 등록
+                Adapter.SelectCommand.Parameters.AddWithValue("iLROW", txtiLROW.Text);
+                Adapter.SelectCommand.Parameters.AddWithValue("ROWNAME", txtRowName.Text);
+                Adapter.SelectCommand.Parameters.AddWithValue("ROWACCOUNT", textRowAccount.Text);
+                Adapter.SelectCommand.Parameters.AddWithValue("ROWSTOCK", textRowStock.Text);
+
+                // 데이터 베이스 처리 시 C#으로 반환할 값을 담는 변수.
+                Adapter.SelectCommand.Parameters.AddWithValue("LANG", "KO");
+                Adapter.SelectCommand.Parameters.AddWithValue("RS_CODE", "").Direction = ParameterDirection.Output;
+                Adapter.SelectCommand.Parameters.AddWithValue("RS_MSG", "").Direction = ParameterDirection.Output;
+
+                // Adapter 실행
+                DataTable dtTemp = new DataTable();
+                Adapter.Fill(dtTemp);
+
+                // 결과값을 그리드뷰에 표현.
+                GridRowMaster.DataSource = dtTemp;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                Connect.Close();
+            }
+        }
+        public bool DBHelper(bool Tran)
+        {
+            // 1. 데이터 베이스 접속
+            Connect = new SqlConnection(Commons.cDbCon);
+
+            // 2. 데이터 베이스 OPEN
+            Connect.Open();
+            if (Connect.State != ConnectionState.Open)
+            {
+                MessageBox.Show("데이터 베이스 연결에 실패하였습니다.");
+                return false;
+            }
+            if (Tran) tran = Connect.BeginTransaction();
+            return true;
+        }
+
+   
     }
 }
